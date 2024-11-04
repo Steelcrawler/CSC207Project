c985959d0830fd181effb7711fcab8c5ea7a8030
Steelcrawler <umailadi@gmail.com>
2024-11-04 13:30:37 -0500

    Added login from lab5

---
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -0,0 +1,17 @@
+package use_case.logout;
+
+/**
+ * The Input Data for the Logout Use Case.
+ */
+public class LogoutInputData {
+
+    private final String currentUsername;
+
+    public LogoutInputData(String username) {
+        this.currentUsername = username;
+    }
+
+    public String getCurrentUsername() {
+        return currentUsername;
+    }
+}
