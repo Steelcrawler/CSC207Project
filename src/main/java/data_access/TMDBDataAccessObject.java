2b78a06f566353fe70f3755613c7e975de1fe2c0
Steelcrawler <umailadi@gmail.com>
2024-11-08 14:00:53 -0500

    Updated pom.xml for http3 dependency, added .env to gitignore, created TMDB Data Access Object file

---
+++ src/main/java/data_access/TMDBDataAccessObject.java
@@ -0,0 +1,22 @@
+package data_access;
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import entity.User;
+import entity.UserFactory;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import use_case.change_password.ChangePasswordUserDataAccessInterface;
+import use_case.login.LoginUserDataAccessInterface;
+import use_case.logout.LogoutUserDataAccessInterface;
+import use_case.signup.SignupUserDataAccessInterface;
+
+
+public class TMDBDataAccessObject {
+    
+}
