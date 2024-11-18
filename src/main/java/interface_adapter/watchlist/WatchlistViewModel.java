4b3d4a1c1598001eaf42b239876270d4f71c1863
DCKuhn <daniel.kuhn@mail.utoronto.ca>
2024-11-18 13:34:59 -0500

    Watchlist View and ViewModel Progress

--- src/main/java/interface_adapter/watchlist/WatchlistViewModel.java
+++ src/main/java/interface_adapter/watchlist/WatchlistViewModel.java
@@ -3,10 +3,8 @@ package interface_adapter.watchlist;
 import interface_adapter.ViewModel;
 import interface_adapter.moviesearch.MovieSearchState;
 
-public class WatchlistViewModel extends ViewModel {
+public class WatchlistViewModel extends ViewModel<WatchlistState> {
     public static final String TITLE_LABEL = "Your Watchlist";
-    public static final String MOVIE_TITLE_LABEL = "Enter a movie title";
-
     public static final String BACK_BUTTON_LABEL = "Back";
     public static final String SELECT_BUTTON_LABEL = "Select";
 
