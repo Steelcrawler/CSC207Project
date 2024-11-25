ce62e2bf9421b7d94e7f6fba3fff967bf2f35a89
DCKuhn <daniel.kuhn@mail.utoronto.ca>
2024-11-25 03:02:42 -0500

    SelectView Swing implemented, other classes the view depends on are yet to be implemented, so currently this doesn't work.

--- src/main/java/view/SelectView.java
+++ src/main/java/view/SelectView.java
@@ -22,7 +22,7 @@ import interface_adapter.login.LoginState;
 //import interface_adapter.moviesearch.MovieSearchState;
 //import interface_adapter.moviesearch.MovieSearchViewModel;
 import interface_adapter.signup.SignupViewModel;
-import interface_adapter.watchlist.WatchlistController;
+import interface_adapter.open_watchlist.OpenWatchlistController;
 import interface_adapter.watchlist.WatchlistState;
 import interface_adapter.watchlist.WatchlistViewModel;
 
@@ -36,8 +36,9 @@ public class SelectView extends JPanel implements ActionListener, ItemListener, 
     private final JScrollPane selectScrollPane;
     private final JPanel moviePanel;
     private final JButton recommendationButton;
+    private final JPanel eastPanel;
 
-    private WatchlistController watchlistController;
+    private SelectController selectController;
 
     public SelectView(SelectViewModel selectViewModel) {
         this.selectViewModel = selectViewModel;
@@ -50,11 +51,17 @@ public class SelectView extends JPanel implements ActionListener, ItemListener, 
         backButton = new JButton(WatchlistViewModel.BACK_BUTTON_LABEL);
         menuPanel.add(backButton, BorderLayout.WEST);
 
+        eastPanel = new JPanel(new BorderLayout());
         deleteButton = new JButton(WatchlistViewModel.SELECT_BUTTON_LABEL);
-        menuPanel.add(deleteButton, BorderLayout.EAST);
-
+        recommendationButton = new JButton("Get a recommendation"); //SelectViewModel isn't implemented yet
+        eastPanel.add(deleteButton, BorderLayout.EAST);
+        JPanel spacer = new JPanel();
+        spacer.setPreferredSize(new Dimension(20, 20));
+        eastPanel.add(spacer, BorderLayout.CENTER);
+        eastPanel.add(recommendationButton, BorderLayout.WEST);
         JLabel titleLabel = new JLabel(WatchlistViewModel.TITLE_LABEL, SwingConstants.CENTER);
         menuPanel.add(titleLabel, BorderLayout.CENTER);
+        menuPanel.add(eastPanel, BorderLayout.EAST);
 
         this.add(menuPanel, BorderLayout.NORTH);
 
@@ -73,145 +80,22 @@ public class SelectView extends JPanel implements ActionListener, ItemListener, 
             moviePanel.add(individualMoviePanel);
         }
 
-        recommendationButton = new JButton("Get a recommendation");
-        recommendationButton.setPreferredSize(new Dimension(110, 140));
-        moviePanel.add(recommendationButton);
-
         selectScrollPane = new JScrollPane(moviePanel);
         selectScrollPane.getVerticalScrollBar().setUnitIncrement(15);
         this.add(selectScrollPane, BorderLayout.CENTER);
     }
-//        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-//        this.setPreferredSize(new Dimension(800, 800));
-//
-//        final JLabel title = new JLabel(watchlistViewModel.TITLE_LABEL);
-//        title.setAlignmentX(Component.CENTER_ALIGNMENT);
-//
-////        final LabelTextPanel titleInfo = new LabelTextPanel(
-////                new JLabel(MovieSearchViewModel.MOVIE_TITLE_LABEL), titleTextField);
-////        final LabelComboBox genreInfo = new LabelComboBox(
-////                new JLabel(MovieSearchViewModel.GENRE_LABEL), genreComboBox);
-////        final LabelComboBox ratingInfo = new LabelComboBox(
-////                new JLabel(MovieSearchViewModel.RATING_LABEL), ratingComboBox);
-//
-//        this.menuPanel = new JPanel();
-//        this.backButton = new JButton(watchlistViewModel.BACK_BUTTON_LABEL);
-//        this.selectButton = new JButton(watchlistViewModel.SELECT_BUTTON_LABEL);
-//        menuPanel.add(backButton);
-//        menuPanel.add(title);
-//        menuPanel.add(selectButton);
-
-//        backButton.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent evt) {
-//                if (evt.getSource().equals(searchButton)) {
-//                    if (resultsTable != null) {
-//                        resultsPanel.remove(resultsTable);
-//                    }
-//                    final MovieSearchState currentState = movieSearchViewModel.getState();
-//                    System.out.println("Title: " + currentState.getTitle());
-//
-//                    movieSearchController.execute(currentState.getTitle());
-//                }
-//            }
-//        });
-//
-//        this.revalidate();
-//        this.repaint();
-
-//        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-
-    //    public String setResultsTable(JTable newResultsTable) {
-//        this.resultsTable = newResultsTable;
-//    }
-//    private void addTitleListener() {
-//        titleTextField.getDocument().addDocumentListener(new DocumentListener() {
-//
-//            private void documentListenerHelper() {
-//                final MovieSearchState currentState = movieSearchViewModel.getState();
-//                currentState.setTitle(titleTextField.getText());
-//                watchlistViewModel.setState(currentState);
-//            }
-//
-//            @Override
-//            public void insertUpdate(DocumentEvent e) {
-//                documentListenerHelper();
-//            }
-//
-//            @Override
-//            public void removeUpdate(DocumentEvent e) {
-//                documentListenerHelper();
-//            }
-//
-//            @Override
-//            public void changedUpdate(DocumentEvent e) {
-//                documentListenerHelper();
-//            }
-//        });
-//    }
-
-//    private void addGenreListener() {
-//        genreComboBox.addItemListener(new ItemListener() {
-//
-//            private void itemListenerHelper() {
-//                final MovieSearchState currentState = movieSearchViewModel.getState();
-//                currentState.setGenre(genreComboBox.getItemAt(genreComboBox.getSelectedIndex()));
-//                movieSearchViewModel.setState(currentState);
-//            }
-//
-//            @Override
-//            public void itemStateChanged(ItemEvent e) {
-//                if (e.getStateChange() == ItemEvent.SELECTED) {
-//                    itemListenerHelper();
-//                }
-//            }
-//        });
-//    }
-//
-//    private void addRatingListener() {
-//        ratingComboBox.addItemListener(new ItemListener() {
-//
-//            private void itemListenerHelper() {
-//                final MovieSearchState currentState = movieSearchViewModel.getState();
-//                currentState.setRating(ratingComboBox.getItemAt(ratingComboBox.getSelectedIndex()));
-//                movieSearchViewModel.setState(currentState);
-//            }
-//
-//            @Override
-//            public void itemStateChanged(ItemEvent e) {
-//                if (e.getStateChange() == ItemEvent.SELECTED) {
-//                    itemListenerHelper();
-//                }
-//            }
-//        });
-//    }
 
     public void propertyChange(PropertyChangeEvent evt) {
         final WatchlistState state = (WatchlistState) evt.getNewValue();
 
     }
 
-//
-//
-////        final String[] columnNames = {"Title", "Genre", "Rating", "Plot Synopsis"};
-////        Object[][] results = {{"xyz", "xyz", "xyz", "xyz"}};
-////        this.add(new JTable(results, columnNames));
-//
-//        if (state.getSearchFound()) {
-//            errorMessageField.setText("");
-//            final String[] columnNames = {"Title", "Genre", "Rating", "Plot Synopsis"};
-//            resultsTable = new JTable(state.getMoviesInfo(), columnNames);
-//            resultsPanel.add(resultsTable);
-//            resultsPanel.revalidate();
-//            resultsPanel.repaint();
-//        }
-//    }
 
     public String getViewName() {
         return viewName;
     }
-    public void setWatchlistController(WatchlistController watchlistController) {
-        this.watchlistController = watchlistController;
+    public void setSelectController(SelectController selectController) {
+        this.selectController = selectController;
     }
 
     @Override
