86170619565618269537d21001a39c2ce3458e90
love2know <1757392691@qq.com>
2024-11-30 16:26:06 -0500

    Justification use case

--- src/main/java/use_case/movie_justif/MovieJustifDataAccessInterface.java
+++ src/main/java/use_case/movie_justif/MovieJustifDataAccessInterface.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.List;
 import entity.Movie;
 
-public interface MovieRecDataAccessInterface {
+public interface MovieJustifDataAccessInterface {
 
     /**
      * Returns a String that is supposed to be similar to 'hello, world!'.
@@ -14,18 +14,19 @@ public interface MovieRecDataAccessInterface {
      */
     String testWork() throws JsonProcessingException;
 
-    /**
-     * Returns the list of recommendations for movies generated in searchMoviesByTitle.
-     * @param movies a list of movies that
-     * @return recommendations and justification generated from gemini.
-     */
-    List<String> genRecommendations(List<Movie> movies) throws Exception;
+//    /**
+//     * Returns the list of recommendations for movies generated in searchMoviesByTitle.
+//     * @param movies a list of movies that
+//     * @return recommendations and justification generated from gemini.
+//     */
+//    List<String> genRecommendations(List<Movie> movies) throws Exception;
 
     /**
      * Returns the justification made by Gemini/Chatgpt why the movie is recommended to the user.
-     * @param movie the recommended movie.
+     * @param wantFrom the movies that the user wants to be recommended from.
+     * @param recommended the movie that is recommended to the user.
      * @return the justification made by Gemini/Chatgpt.
      * @throws Exception
      */
-    String getJustification(Movie movie) throws Exception;
+    String getJustification(List<String> wantFrom, String recommended) throws Exception;
 }
--- src/main/java/use_case/movie_justif/MovieJustifInteractor.java
+++ src/main/java/use_case/movie_justif/MovieJustifInteractor.java
@@ -1,5 +1,65 @@
 package use_case.movie_justif;
 
-public class MovieRecInteractor {
+import entity.Movie;
+import use_case.movie_info.MovieInfoDataAccessInterface;
+import interface_adapter.movie_justif.MovieJustifState;
+import interface_adapter.movie_search.MovieSearchState;
+import view.ButtonEditor;
+import view.ButtonRenderer;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Movie Justif Interactor.
+ */
+public class MovieJustifInteractor implements MovieJustifInputBoundary {
+    private final MovieInfoDataAccessInterface TMDBDataAccessObject;
+    private final MovieJustifDataAccessInterface GeminiDataAccessObject;
+    private final MovieJustifOutputBoundary movieJustifPresenter;
+
+    public MovieJustifInteractor(MovieInfoDataAccessInterface TMDBDataAccessObject,
+            MovieJustifDataAccessInterface movieJustifDataAccessInterface,
+                               MovieJustifOutputBoundary movieJustifOutputBoundary) {
+        this.TMDBDataAccessObject = TMDBDataAccessObject;
+        this.GeminiDataAccessObject = movieJustifDataAccessInterface;
+        this.movieJustifPresenter = movieJustifOutputBoundary;
+    }
+
+    @Override
+    public void execute(MovieJustifInputData movieJustifInputData) {
+        int rec = movieJustifInputData.getRecommended();
+        Movie movie = TMDBDataAccessObject.getMovieByID(rec);
+        String justif = "Justification not available yet.";
+        List<String> titles = new ArrayList<>();
+        for (int id: movieJustifInputData.getWantFrom()) {
+            Movie m = TMDBDataAccessObject.getMovieByID(id);
+            titles.add(m.getTitle());
+        }
+        try {
+            String justification = GeminiDataAccessObject.getJustification(titles, movie.getTitle());
+            justif = justification;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(justif);
+        String movieTitle = movie.getTitle();
+        double movieRating = movie.getRating();
+        String moviePoster = movie.getPosterPath();
+        String movieTrailer = movie.getTrailerLink();
+        List<String> movieReviews = movie.getUserReviews();
+        final MovieJustifOutputData movieJustifOutputData = new MovieJustifOutputData(movieTitle, movieRating,
+                justif, moviePoster, movieTrailer, movieReviews);
+        this.movieJustifPresenter.prepareSuccessView(movieJustifOutputData);
+    }
+
+    @Override
+    public void switchView() {
+        this.movieJustifPresenter.switchToRecView();
+    }
 }
