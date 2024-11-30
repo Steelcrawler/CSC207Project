dd438b396142867871c2eb91aa1cc750817dfca0
Alaya Le <alaya.le@mail.utoronto.ca>
2024-11-30 02:12:05 -0500

    Fixed checkstyle AddToWatchlistInteractor and edited AddToWatchlistInteractorTest to use the InMemoryUserDataAccessObject

--- src/main/java/use_case/add_to_watchlist/AddToWatchlistInteractor.java
+++ src/main/java/use_case/add_to_watchlist/AddToWatchlistInteractor.java
@@ -3,7 +3,7 @@ package use_case.add_to_watchlist;
 /**
  * The Add To Watchlist Interactor.
  */
-public class AddToWatchlistInteractor implements AddToWatchlistInputBoundary {
+public abstract class AddToWatchlistInteractor implements AddToWatchlistInputBoundary, AddToWatchlistOutputBoundary {
     private final AddToWatchlistDataAccessInterface MongoDBDataAccessObject;
     private final AddToWatchlistOutputBoundary addToWatchlistPresenter;
 
@@ -15,11 +15,13 @@ public class AddToWatchlistInteractor implements AddToWatchlistInputBoundary {
 
     @Override
     public void execute(AddToWatchlistInputData addToWatchlistInputData) {
-        if (!MongoDBDataAccessObject.existsInWatchlist(MongoDBDataAccessObject.getCurrentUsername(), addToWatchlistInputData.getMovieID())) {
-            System.out.println("Movie isn't in watchlist yet");
-            MongoDBDataAccessObject.addToWatchlist(MongoDBDataAccessObject.getCurrentUsername(), addToWatchlistInputData.getMovieID());
+        if (!MongoDBDataAccessObject.existsInWatchlist(MongoDBDataAccessObject.getCurrentUsername(),
+                addToWatchlistInputData.getMovieID())) {
+            MongoDBDataAccessObject.addToWatchlist(MongoDBDataAccessObject.getCurrentUsername(), addToWatchlistInputData
+                    .getMovieID());
 
-            final AddToWatchlistOutputData addToWatchlistOutputData = new AddToWatchlistOutputData(addToWatchlistInputData.getMovieTitle(), false);
+            final AddToWatchlistOutputData addToWatchlistOutputData = new AddToWatchlistOutputData(
+                    addToWatchlistInputData.getMovieTitle(), false);
             this.addToWatchlistPresenter.prepareSuccessView(addToWatchlistOutputData);
             }
         else {
