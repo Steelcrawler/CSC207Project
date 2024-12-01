92c1da5e7a5dd9dac58cc95dccfc36eb6469e3ca
DCKuhn <daniel.kuhn@mail.utoronto.ca>
2024-12-01 14:27:07 -0500

    Tests Done

--- src/main/java/use_case/recommendation/RecommendationInteractor.java
+++ src/main/java/use_case/recommendation/RecommendationInteractor.java
@@ -23,43 +23,49 @@ public class RecommendationInteractor implements RecommendationInputBoundary {
 
     @Override
     public void execute(RecommendationInputData recommendationInputData) {
-        if (TMDBDataAccessObject != null) {
-            List<Movie> moviesList;
-            if (recommendationInputData.getSelectedMoviesList().size() > 1) {
-                List<Integer> keywordsList = new ArrayList<Integer>();
-                for (Integer movieID : recommendationInputData.getSelectedMoviesList()) {
-                    keywordsList.addAll(this.TMDBDataAccessObject.getKeywordIDs(movieID));
-                }
-                moviesList = this.TMDBDataAccessObject.searchMoviesByKeywordsHelper(keywordsList);
-            } else {
+        List<Movie> moviesList;
+        if (recommendationInputData.getSelectedMoviesList().size() > 1) {
+            List<Integer> keywordsList = new ArrayList<Integer>();
+            for (Integer movieID : recommendationInputData.getSelectedMoviesList()) {
+                keywordsList.addAll(this.TMDBDataAccessObject.getKeywordIDs(movieID));
+            }
+            moviesList = this.TMDBDataAccessObject.searchMoviesByKeywordsHelper(keywordsList);
+        } else {
+            try {
                 moviesList = this.TMDBDataAccessObject.searchRecommendations(recommendationInputData.getSelectedMoviesList());
-            }
-            List<Integer> movieIDsOutput = new ArrayList<Integer>();
-            List<String> movieTitlesOutput = new ArrayList<String>();
-            List<String> posterPathsOutput = new ArrayList<String>();
-            List<String> plotsOutput = new ArrayList<String>();
-            List<Integer> selectedIDsOutput = new ArrayList<Integer>(recommendationInputData.getSelectedMoviesList());
-
-            for (Movie movie : moviesList) {
-                movieIDsOutput.add(movie.getMovieID());
-                movieTitlesOutput.add(movie.getTitle());
-                posterPathsOutput.add(movie.getPosterPath());
-                plotsOutput.add(movie.getPlot());
-            }
-            if (movieIDsOutput.size() == 0) {
-                recommendationPresenter.prepareFailView("A Recommendation could not be made");
-            } else {
-                RecommendationOutputData recommendationOutputData = new RecommendationOutputData(movieIDsOutput,
-                        movieTitlesOutput,
-                        posterPathsOutput,
-                        plotsOutput,
-                        selectedIDsOutput,
-                        false);
-                this.recommendationPresenter.prepareSuccessView(recommendationOutputData);
+            } catch (IndexOutOfBoundsException e) {
+                moviesList = new ArrayList<>();
+                recommendationPresenter.prepareFailView("Movies were not provided");
             }
         }
-    }
+        List<Integer> movieIDsOutput = new ArrayList<Integer>();
+        List<String> movieTitlesOutput = new ArrayList<String>();
+        List<String> posterPathsOutput = new ArrayList<String>();
+        List<String> plotsOutput = new ArrayList<String>();
+        List<Integer> selectedIDsOutput = new ArrayList<Integer>(recommendationInputData.getSelectedMoviesList());
 
+        for (Movie movie : moviesList) {
+            movieIDsOutput.add(movie.getMovieID());
+            movieTitlesOutput.add(movie.getTitle());
+            posterPathsOutput.add(movie.getPosterPath());
+            plotsOutput.add(movie.getPlot());
+        }
+        System.out.println(movieIDsOutput);
+        System.out.println(movieTitlesOutput);
+        System.out.println(posterPathsOutput);
+        System.out.println(plotsOutput);
+        if (movieIDsOutput.size() == 0) {
+            recommendationPresenter.prepareFailView("A Recommendation could not be made");
+        } else {
+            RecommendationOutputData recommendationOutputData = new RecommendationOutputData(movieIDsOutput,
+                    movieTitlesOutput,
+                    posterPathsOutput,
+                    plotsOutput,
+                    selectedIDsOutput,
+                    false);
+            this.recommendationPresenter.prepareSuccessView(recommendationOutputData);
+        }
+    }
     @Override
     public void toSelectView() {
         recommendationPresenter.toSelectView();
